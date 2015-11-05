2fd2325f2d49332c2cf4a2375924fc8725b5cadb
Lee <Lee@192.168.242.1>
2015-11-04 19:19:46 -0800

    now data base is linked to code add customer seach customer

---
+++ NDS[DB]/src/Main.java
@@ -0,0 +1,53 @@
+import java.sql.*;
+public class Main {
+
+//	static Connection con;
+//	static Statement stmt;
+	
+	public static void main(String[] args) {
+		connect cn = new connect();
+		ResultSet r = cn.searchCustomer(0, "", "Do");
+		try{
+			while(r.next()){
+				System.out.println(r.getString("FirstName") + " " + r.getString("LastName"));
+			}
+		}
+		catch(Exception e){}
+//		try{
+//			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ndsdb", "root", "12345");
+//			stmt = con.createStatement();
+//			ResultSet rs = stmt.executeQuery("select * from CUSTOMER");
+//			System.out.println("Database before insert");
+//			while(rs.next()){
+//				System.out.println(rs.getString("FirstName") + " " + rs.getString("LastName"));
+//			}
+//			
+//			addCustomer("Jane", "T", "11111 S Rd", "", "City", "State", "91919", "7654321");
+//			
+//			ResultSet rs2 = stmt .executeQuery("select * from CUSTOMER");
+//			System.out.println("Database after insert");
+//			while(rs2.next()){
+//				System.out.println(rs2.getString("FirstName") + " " + rs2.getString("LastName"));
+//			}
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
+//		
+//		
+//	}
+//	public static void addCustomer(String first, String last, String address, String address2, String city, String state, String zip, String phone){
+//		try{
+//			String add = "insert into CUSTOMER (FirstName, LastName, Address, City, State, Zip, Phone)"
+//					+ " values (\"" + first + "\", \"" + last + "\", \"" + address +  "\", \"" + city + 
+//					"\", \"" + state + "\", \"" + zip + "\", \"" + phone + "\")";
+//			System.out.println(add);
+//			stmt.executeUpdate(add);
+//		}catch(Exception e){
+//			
+//		}
+	}
+//	public static void modifyCustomer(){
+//			
+//	
+//	}
+}
--- NDS_1/src/connect.java
+++ NDS_1/src/connect.java
@@ -1,4 +1,14 @@
+import java.sql.*;
 
 public class connect {
-
+	static Connection con;
+	static Statement stmt;
+	
+	public connect(){
+		try{
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ndsdb", "root", "12345");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
